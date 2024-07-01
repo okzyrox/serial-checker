58796b9ec331b96b127a521a79aaa32b58075cf5
ZyroX <79484310+okzyrox@users.noreply.github.com>
2024-07-02 00:33:57 +0100

    refactor
    no need for this but i kept thinking how awful this looked so i had to

--- checker.py
+++ checker.py
@@ -1,11 +1,11 @@
 import json
 import os, sys
 
-types = [
+serial_types = [
     "XAW1", "XAW4", "XAW7",
     "XAJ1", "XAJ4", "XAJ7",
 ]
-other_types = [
+serial_extra_types = [
     "XKW1", "XKJ1", "XJW1",
     "XWW1", "XAW9", "XAK"
 ]
@@ -13,18 +13,18 @@ other_types = [
 #XAK - korean only - unknown
 #XAW9 - sold from nintendo directly via refurbishment - possibly patched, but mostly unpatched from data recorded
 
-c = True
-skipCheck = False
+debug = True
+skip_check = False
 
 with open("serials.json", "r") as serial_json:
     serial_list = json.load(serial_json)
 
 
 print("Valid First types")
-for i in types:
+for i in serial_types:
     print(i)
 print("Other types")
-for i in other_types:
+for i in serial_extra_types:
     print(i)
 
 serial_invalid = True
@@ -39,81 +39,79 @@ while serial_invalid:
 
     print("Entered:", usrtype)
 
-    if usrtype in types:
+    if usrtype in serial_types:
         serial_invalid = False
-    elif usrtype in other_types:
+    elif usrtype in serial_extra_types:
         match usrtype:
             case "XKW1" | "XKJ1":
                 print("You have a 'Mariko' switch, which has been patched unfortunately")
-                skipCheck = True
-                serial_invalid = False
             case "XAK":
                 print("You own a Korean nintendo switch, not much info is known about them, so it may or may not be patched.")
-                skipCheck = True
-                serial_invalid = False
             case "XAW9":
                 print("You own a refurbished Nintendo Switch sold directly from Nintendo, they're probably patched if it was refurbished before the switch's \n 'Mariko' mainline switch patch, however false positives have been more likely with these versions")
-                skipCheck = True
-                serial_invalid = False
             case "XWW1":
                 print("Your Switch is 100% Patched")
-                skipCheck = True
-                serial_invalid = False
+
+        skip_check = True
+        serial_invalid = False       
     else:
         print("Invalid Serial Code")
 
-if skipCheck == False:
+if skip_check == False:
     print("Enter the remaining digits of your Switch's serial code following the first four letters/numbers ")
     print("This does not include the first four numbers/letters, such as XAJ4 or XAW1")
     sixdig = input()
 
 
-    unpatched_l = []
-    probpatch_l = []
-    patch_l = []
-    unpatched_l.append(serial_list[usrtype]['unpatched']['min'])
-    unpatched_l.append(serial_list[usrtype]['unpatched']['max'])
-    unpatched_l.append(usrtype + sixdig)
+    unpatched_range = []
+    possibly_patched_range = []
+    patched_range = []
 
-    probpatch_l.append(serial_list[usrtype]['possibly patched']['min'])
-    probpatch_l.append(serial_list[usrtype]['possibly patched']['max'])
-    probpatch_l.append(usrtype + sixdig)
+    unpatched_range.append(serial_list[usrtype]['unpatched']['min'])
+    unpatched_range.append(serial_list[usrtype]['unpatched']['max'])
+    unpatched_range.append(usrtype + sixdig)
 
-    patch_l.append(serial_list[usrtype]['patched']['min'])
-    patch_l.append(usrtype + sixdig)
+    possibly_patched_range.append(serial_list[usrtype]['possibly patched']['min'])
+    possibly_patched_range.append(serial_list[usrtype]['possibly patched']['max'])
+    possibly_patched_range.append(usrtype + sixdig)
 
-    unpatched_l.sort() # sort alphanumerically, if serial is inbetween it will go as item 2 [1] and therefore verify it
+    patched_range.append(serial_list[usrtype]['patched']['min'])
+    patched_range.append(usrtype + sixdig)
+
+    unpatched_range.sort() 
+    # sort alphanumerically, if serial is inbetween it will go as item 2 [1] and therefore verify it
     # if it is entered as item 3 [2], then we try again with 'probably patched' and 'patched'
-    probpatch_l.sort()
-    patch_l.sort()
+    possibly_patched_range.sort()
+    patched_range.sort()
 
-    if c:
-        print(unpatched_l)
-        print(probpatch_l)
-        print(patch_l)
+    if debug:
+        print(unpatched_range)
+        print(possibly_patched_range)
+        print(unpatched_range)
 
-    n = usrtype + sixdig
+    userserial = usrtype + sixdig
 
-    if unpatched_l[1] == n:
+    # if its in the middle of unpatched, then its certain to be unpatched
+    if unpatched_range[1] == userserial:
         print("Congrats, your switch isn't patched!")
         x = input("Want to double check? (Y/N)")
         if x.upper() == "Y":
-            if probpatch_l[1] == n:
+            if possibly_patched_range[1] == userserial:
                 print("Possibly patched from check 2")
             else:
                 print("No false positives found from check 2")
             
-            if patch_l[1] == n: # last item, since it will always be higher than the first if 100% patched
+            if patched_range[1] == userserial: # last item, since it will always be higher than the first if 100% patched
                 print("Patched from check 3")
             else:
                 print("No false positives found from check 3")
     else:
-        if probpatch_l[1] == n:
+        if possibly_patched_range[1] == userserial:
             print("Your Switch is possibly patched")
-        if patch_l[1] == n:
+        if patched_range[1] == userserial:
             print("Your Switch is most-likely patched")
         else:
             print("Your Switch's patchness is Unknown")
 
 else:
-    pass
+    print("Finished execution")
--- checker_gui.py
+++ checker_gui.py
@@ -3,7 +3,7 @@ import json
 import tkinter as tk
 from tkinter import messagebox
 
-c = True
+debug = True
 
 with open("serials.json", "r") as serial_json:
     serial_list = json.load(serial_json)
@@ -18,85 +18,83 @@ type_serial = tk.StringVar()
 serial_int = tk.StringVar() # IntVar() bad
 
 
-options = [
+serial_type_options = [
     "XAW1", "XAW4", "XAW7",
     "XAJ1", "XAJ4", "XAJ7",
     "XKW1", "XKJ1", "XJW1",
     "XWW1", "XAW9", "XAK"
 ]
 
-drop = tk.OptionMenu( root , type_serial , *options )
+drop = tk.OptionMenu( root , type_serial , *serial_type_options )
 
-def serial_compat(serialtype, serialcode):
+def serial_compat(serial_type, serial_code):
 
-    skipCheck = False
+    skip_check = False
 
-    usrtype = serialtype
-    usrcode = serialcode
+    usrtype = serial_type
+    usrcode = serial_code
 
-    if usrtype in options:
+    if usrtype in serial_type_options:
         match usrtype:
             case "XKW1" | "XKJ1":
-                messagebox.showerror("Serial Checker","You have a 'Mariko' switch, which has been patched unfortunately")
-                skipCheck = True
-                serial_invalid = False
+                messagebox.showerror("Serial Checker", "You have a 'Mariko' switch, which has been patched unfortunately")
             case "XAK":
-                messagebox.showwarning("Serial Checker","You own a Korean nintendo switch, not much info is known about them, so it may or may not be patched.")
-                skipCheck = True
-                serial_invalid = False
+                messagebox.showwarning("Serial Checker", "You own a Korean nintendo switch, not much info is known about them, so it may or may not be patched.")
             case "XAW9":
-                messagebox.showwarning("Serial Checker","You own a refurbished Nintendo Switch sold directly from Nintendo, they're probably patched if it was refurbished before the switch's \n 'Mariko' mainline switch patch, however false positives have been more likely with these versions")
-                skipCheck = True
-                serial_invalid = False
+                messagebox.showwarning("Serial Checker", "You own a refurbished Nintendo Switch sold directly from Nintendo, they're probably patched if it was refurbished before the switch's \n 'Mariko' mainline switch patch, however false positives have been more likely with these versions")
             case "XWW1":
-                messagebox.showerror("Serial Checker","Your Switch is 100% Patched")
-                skipCheck = True
-                serial_invalid = False
+                messagebox.showerror("Serial Checker", "Your Switch is 100% Patched")
+                
+        skip_check = True
+        serial_invalid = False
 
-    if skipCheck == False:
+    if skip_check == False:
 
-        unpatched_l = []
-        probpatch_l = []
-        patch_l = []
-        unpatched_l.append(serial_list[usrtype]['unpatched']['min'])
-        unpatched_l.append(serial_list[usrtype]['unpatched']['max'])
-        unpatched_l.append(usrtype + usrcode)
+        unpatched_range = []
+        possibly_patched_range = []
+        patched_range = []
 
-        probpatch_l.append(serial_list[usrtype]['possibly patched']['min'])
-        probpatch_l.append(serial_list[usrtype]['possibly patched']['max'])
-        probpatch_l.append(usrtype + usrcode)
+        unpatched_range.append(serial_list[usrtype]['unpatched']['min'])
+        unpatched_range.append(serial_list[usrtype]['unpatched']['max'])
+        unpatched_range.append(usrtype + usrcode)
 
-        patch_l.append(serial_list[usrtype]['patched']['min'])
-        patch_l.append(usrtype + usrcode)
+        possibly_patched_range.append(serial_list[usrtype]['possibly patched']['min'])
+        possibly_patched_range.append(serial_list[usrtype]['possibly patched']['max'])
+        possibly_patched_range.append(usrtype + usrcode)
 
-        unpatched_l.sort() # sort alphanumerically, if serial is inbetween it will go as item 2 [1] and therefore verify it
+        patched_range.append(serial_list[usrtype]['patched']['min'])
+        patched_range.append(usrtype + usrcode)
+
+        unpatched_range.sort() 
+        # sort alphanumerically, if serial is inbetween it will go as item 2 [1] and therefore verify it
         # if it is entered as item 3 [2], then we try again with 'probably patched' and 'patched'
-        probpatch_l.sort()
-        patch_l.sort()
+        possibly_patched_range.sort()
+        patched_range.sort()
 
-        if c:
-            print(unpatched_l)
-            print(probpatch_l)
-            print(patch_l)
+        if debug:
+            print(unpatched_range)
+            print(possibly_patched_range)
+            print(patched_range)
 
-        n = usrtype + usrcode
+        userserial = usrtype + usrcode
 
-        if unpatched_l[1] == n:
+        if unpatched_range[1] == userserial:
             
-            if probpatch_l[1] == n:
+            if possibly_patched_range[1] == userserial:
                 print("Possibly patched from check 2")
                 checktwofine = False
             else:
                 print("No false positives found from check 2")
                 checktwofine = True
                 
-            if patch_l[1] == n: # last item, since it will always be higher than the first if 100% patched
+            if patched_range[1] == userserial: # last item, since it will always be higher than the first if 100% patched
                 print("Patched from check 3")
                 checkthreefine = False
             else:
                 print("No false positives found from check 3")
                 checkthreefine = True
             
+            # what amazing code i wrote (not)
             if checktwofine and checkthreefine:
                 messagebox.showinfo("Serial Checker","Congrats, your switch isn't patched!\n (No issues found with double-checks either)")
             elif checktwofine and not checkthreefine:
@@ -108,9 +106,9 @@ def serial_compat(serialtype, serialcode):
             else:
                 messagebox.showinfo("Serial Checker", "Error when parsing serial number")
         else:
-            if probpatch_l[1] == n:
+            if possibly_patched_range[1] == userserial:
                 messagebox.showwarning("Serial Checker","Your Switch is possibly patched")
-            if patch_l[1] == n:
+            if patched_range[1] == userserial:
                 messagebox.showerror("Serial Checker","Your Switch is most-likely patched")
             else:
                 messagebox.showinfo("Serial Checker","Your Switch's patchness is Unknown")
@@ -121,8 +119,8 @@ def serial_compat(serialtype, serialcode):
 
 def window_submit_serials():
 
-    serial_type_submit=type_serial.get()
-    serial_int_submit=serial_int.get()
+    serial_type_submit = type_serial.get()
+    serial_int_submit = serial_int.get()
 
     print(serial_type_submit)
     print(serial_int_submit)
@@ -134,17 +132,17 @@ def window_submit_serials():
 
 
 serial_type_name = tk.Label(root, text = 'Serial Type', font=('calibre',10, 'bold'))
-serial_type_entry = tk.Entry(root,textvariable = type_serial, font=('calibre',10,'normal'))
+serial_type_entry = tk.Entry(root, textvariable = type_serial, font=('calibre',10,'normal'))
 
 serial_int_name = tk.Label(root, text = 'Serial Number (6-10)', font = ('calibre',10,'bold'))
 serial_int_entry = tk.Entry(root, textvariable = serial_int, font = ('calibre',10,'normal'))
 
 sub_btn=tk.Button(root,text = 'Submit', command = window_submit_serials)
 
-serial_type_name.grid(row=0,column=0)
+serial_type_name.grid(row=0, column=0)
 drop.grid(row=0, column=1)
-serial_int_name.grid(row=1,column=0)
-serial_int_entry.grid(row=1,column=1)
-sub_btn.grid(row=2,column=1)
+serial_int_name.grid(row=1, column=0)
+serial_int_entry.grid(row=1, column=1)
+sub_btn.grid(row=2, column=1)
 
 root.mainloop()
